c9e05258d74d31b22386da3e636a33c17f0d4262
hazemgem2021 <hazemgem2021@outlook.com>
2023-02-17 13:38:03 -0800

    v5

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,9 @@ int main(void)
 			}
 			else if (n < 0)
 		        {
-		       	   printf("%d is negative/n", n);
+		       	        printf("%d is negative/n", n);
 			}
+
 			else
 			{       printf("%d is zero/n", n);
 
