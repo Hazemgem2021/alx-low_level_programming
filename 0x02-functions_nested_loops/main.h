eace34775418067706e4c694c0648518ca8f510c
hazemgem2021 <hazemgem2021@outlook.com>
2023-02-21 11:33:24 -0800

    main2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,10 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 /*
  * File: main.h
  *
  * Desc: Header file containing declartions for all function
  */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -19,5 +18,4 @@ void time_teble(void);
 int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
-
-#endif 
+#endif
