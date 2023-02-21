e4809734cbbdec053f84efe5148876fffbc0dc7a
hazemgem2021 <hazemgem2021@outlook.com>
2023-02-21 11:20:10 -0800

    First

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+char abe[] = "putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+putchar(abe[c]);
+}
+putchar('\n');
+return (0);
+}
