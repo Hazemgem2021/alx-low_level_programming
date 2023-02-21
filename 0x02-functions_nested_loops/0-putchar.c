3985ca010d7ab3a0578f40aa4574e0084359955d
hazemgem2021 <hazemgem2021@outlook.com>
2023-02-21 11:37:04 -0800

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
+ *  main - prints _putchar, fllowd by a new linees
+ *  Return: Alwys 0 (Success)
  */
 int main(void)
 {
-char abe[] = "putchar";
+char abe[] = "_putchar";
 int c;
 for (c = 0; c < 8; c++)
 {
-putchar(abe[c]);
+_putchar(abe[c]);
 }
-putchar('\n');
+_putchar('\n');
 return (0);
 }
