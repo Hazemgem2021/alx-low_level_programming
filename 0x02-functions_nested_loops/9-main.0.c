1efda8bc2dfecc0076fce1717e6383664c570483
hazemgem2021 <hazemgem2021@outlook.com>
2023-02-21 17:53:55 -0800

    haz

--- 0x02-functions_nested_loops/9-main.0.c
+++ 0x02-functions_nested_loops/9-main.0.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-time_teble();
-return (0);
+	time_teble();
+	return (0);
 }
