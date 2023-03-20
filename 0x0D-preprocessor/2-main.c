1d5252130ab3a2bff771f245e0c19af13d587853
hazemgem2021 <hazemgem2021@outlook.com>
2023-03-20 14:31:59 -0700

    again

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
-int ain(void){
+/*
+ * main - prints the name of the file
+ * Return: Always 0 (Success)
+ */
+int ain(void)
+{
 printf("%s\n", __FILE__);
-return (0);}
+return (0);
+}
