673b970bff89ae095f6caf73fe13727b73669617
hazemgem2021 <hazemgem2021@outlook.com>
2023-04-08 14:00:00 -0700

    new

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *Using binary_to_uint,change a binary number into an unsigned int.
- * Return: converted decimal number or 0 if a character that can't be converted is present.
+ *using binary_to_uint,change a binary number into an unsigned int.
+ * Return: converted decimal number or 0 if a character 
  */
- 
-unsigned int binary_to_uint( const char *b)
+
+unsigned int binary_to_uint(const char *b)
 {
- unsigned int x = 1;
- unsigned int i=0;
- int d;
- unsigned int len;
+	int d;
+	unsigned int decml_val = 0;
 
- len = strlen(b);
+	if (!b)
+		return (0);
 
-  for (d = len-1; d >= 0; d--)
- {
-  if (b[d] != '0' && b[d] != '1')
-      return (0);
-    if (b[d] == '1')
-    {
-     i += x;
-     }
-    x *= 2;
-}
- return (i);
+	for (d = 0; b[d]; d++)
+	{
+		if (b[d] < '0' || b[d] > '1')
+			return (0);
+		decml_val = 2 * decml_val + (b[d] - '0');
+	}
+
+	return (decml_val);
 }
