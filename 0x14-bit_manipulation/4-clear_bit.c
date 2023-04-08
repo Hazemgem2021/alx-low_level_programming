d2af0961ce385949eaa625d7f726f655712a9424
hazemgem2021 <hazemgem2021@outlook.com>
2023-04-08 16:50:19 -0700

    until i win

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: parameter
  * @index: index
- * Return: 1 for success, -1 for failure 
+ * Return: 1 for success, -1 for failure
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
